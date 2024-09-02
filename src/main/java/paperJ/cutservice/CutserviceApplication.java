9e07f39498129364040f9a282fcdf160ce039edb
eusan <goodsan0330@gamil.com>
2024-09-03 01:12:27 +0900

    Generate project & Setting

---
+++ src/main/java/paperJ/cutservice/CutserviceApplication.java
@@ -0,0 +1,13 @@
+package paperJ.cutservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CutserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CutserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/paperJ/cutservice/CutserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package paperJ.cutservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CutserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
